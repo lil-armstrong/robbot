8f71422a29039d122723ccac8b0527ecf66c7b4a
lil-armstrong <oebong1@gmail.com>
2021-06-28 16:59:53 +0100

    new heroku deploy

--- config/http.js
+++ config/http.js
@@ -55,6 +55,5 @@ module.exports.http = {
     //   return middlewareFn;
     // })(),
   },
-  trustProxy: true
 
 };
--- config/session.js
+++ config/session.js
@@ -35,7 +35,5 @@ module.exports.session = {
   // isSessionDisabled: function (req){
   //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
   // },
-    cookie:{
-      secure: true
-    }
+    
 };
